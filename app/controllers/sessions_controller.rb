91a3b18f035cb2fed90f91700d717a7c51513d76
oscarpuga <oscar.puga.rey@gmail.com>
2016-04-24 23:54:34 +0000

    rq10

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,22 @@ class SessionsController < ApplicationController
   end
 
   def create
+    username = params[:username]
+    password = params[:password]
+    user = User.where(username:"#{username}").first
+    if user.authenticate("#{password}")
+      session[:userid] = user.id
+      flash[:notice] = "successfully logged"
+      redirect_to root_path
+    else
+      flash[:alert] = "wrong password"
+      redirect_to login_path
+    end
   end
 
   def destroy
+    reset_session
+    flash[:notice] = "successfully logged out"
+    redirect_to login_path
   end
 end
